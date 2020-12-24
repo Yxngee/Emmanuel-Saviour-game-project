10143a93cc0e488c4bb44b776f840922b5d1cd1e
JosephMwayi00 <josephmulowayi14@gmail.com>
2020-12-24 02:46:25 +0000

    end

--- Joseph Mulowayi - Personal Project/Assets/Scripts/UrlLinks.cs
+++ Joseph Mulowayi - Personal Project/Assets/Scripts/UrlLinks.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UrlLinks : MonoBehaviour
 {
+    public string url = "https://github.com/JosephMwayi00/B00130551_JosephMulowayi_Final-Project";
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void loadUrl()
+    {
+        Application.OpenURL(url);
     }
 }
